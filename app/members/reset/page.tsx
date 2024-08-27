a37f7ca7b997c00f212bca586fc9f9becd61634d
MinhTri <hminhtri03@gmail.com>
2024-08-27 14:43:03 +0700

    Remove large file and update .gitignore

--- app/members/reset/page.tsx
+++ app/members/reset/page.tsx
@@ -1,47 +1,47 @@
-import React from 'react';
+// import React from 'react';
 
-import { type Metadata } from 'next';
+// import { type Metadata } from 'next';
 
-// components
-import Master from '@components/Layout/Master';
-import Section from '@components/Section/Section';
-import Heading from '@components/Heading/Heading';
+// // components
+// import Master from '@components/Layout/Master';
+// import Section from '@components/Section/Section';
+// import Heading from '@components/Heading/Heading';
 
-import Form from './components/Form';
+// import Form from './components/Form';
 
-const Page = (): React.JSX.Element => (
-  <Master>
-    <Section className='white-background'>
-      <div className='container'>
-        <div className='center'>
-          <Heading type={1} color='gray' text='Reset password' />
-          <p className='gray form-information'>
-            Please enter your new password. We will change your password immediately.
-          </p>
-        </div>
-        <Form />
-      </div>
-    </Section>
-  </Master>
-);
+// const Page = (): React.JSX.Element => (
+//   <Master>
+//     <Section className='white-background'>
+//       <div className='container'>
+//         <div className='center'>
+//           <Heading type={1} color='gray' text='Reset password' />
+//           <p className='gray form-information'>
+//             Please enter your new password. We will change your password immediately.
+//           </p>
+//         </div>
+//         <Form />
+//       </div>
+//     </Section>
+//   </Master>
+// );
 
-const title = 'Reset password';
-const canonical = 'https://modern-ticketing.com/members/reset';
-const description = 'Ticketer is a Ticketer solution';
+// const title = 'Reset password';
+// const canonical = 'https://modern-ticketing.com/members/reset';
+// const description = 'Ticketer is a Ticketer solution';
 
-export const metadata: Metadata = {
-  title,
-  description,
-  keywords: 'Ticketer',
-  alternates: { canonical },
-  openGraph: {
-    title,
-    description,
-    url: canonical,
-    type: 'website',
-    siteName: 'Ticketer',
-    images: 'https://modern-ticketing.com/logo192.png',
-  },
-};
+// export const metadata: Metadata = {
+//   title,
+//   description,
+//   keywords: 'Ticketer',
+//   alternates: { canonical },
+//   openGraph: {
+//     title,
+//     description,
+//     url: canonical,
+//     type: 'website',
+//     siteName: 'Ticketer',
+//     images: 'https://modern-ticketing.com/logo192.png',
+//   },
+// };
 
-export default Page;
+// export default Page;
